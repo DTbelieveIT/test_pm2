9eb0b753db3af3aba582bda17f79b61c913a0172
GUO DT <gdtbelievefuture@163.com>
2017-04-18 12:55:56 +0800

    update the response
    change the reponse word in third time！

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 var http = require('http');
 http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' }); res.end('Hello World,the second time!\n');                                                                                                                         
+    res.writeHead(200, { 'Content-Type': 'text/plain' }); res.end('Hello World,the third time!\n');                                                                                                                         
 }).listen(8888, "127.0.0.1"); 
 console.log('Server running at localhost');
